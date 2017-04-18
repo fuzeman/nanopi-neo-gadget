189fa7ccac30381ff2efb9607d8192241c2b86e2
Dean Gardiner <me@dgardiner.net>
2017-04-19 11:53:55 +1200

    Updated [.travis/decrypt.sh] script

--- .travis/decrypt.sh
+++ .travis/decrypt.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+
+# Ensure .snapcraft/ directory exists
+mkdir .snapcraft/
+
+# Decrypt snapcraft credentials
 openssl aes-256-cbc \
   -K $encrypted_f4fc35a3a47c_key \
   -iv $encrypted_f4fc35a3a47c_iv \
