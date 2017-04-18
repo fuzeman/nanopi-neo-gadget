20179f8dd3f8f810961f11e701bd9127848f1b13
Dean Gardiner <me@dgardiner.net>
2017-04-19 11:48:53 +1200

    Updated [.travis/decrypt.sh] script

--- .travis/decrypt.sh
+++ .travis/decrypt.sh
@@ -2,5 +2,5 @@
 openssl aes-256-cbc \
   -K $encrypted_f4fc35a3a47c_key \
   -iv $encrypted_f4fc35a3a47c_iv \
-  -in .snapcraft/travis_snapcraft.cfg \
+  -in .travis/snapcraft.cfg \
   -out .snapcraft/snapcraft.cfg -d
